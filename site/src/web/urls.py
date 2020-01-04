60de59bd33004ace853cd9bd564963076affe6c0
Yunxiang Chang <william.chang@uwaterloo.ca>
2020-01-03 19:33:19 -0500

    remove unnecessary slash

--- site/src/web/urls.py
+++ site/src/web/urls.py
@@ -3,5 +3,5 @@ from src.web.base.views import LogoutView
 
 urlpatterns = [
     path('', include('src.web.home.urls')),
-    path('/logout', LogoutView.as_view(), name='logout_view'),
+    path('logout', LogoutView.as_view(), name='logout_view'),
 ]
