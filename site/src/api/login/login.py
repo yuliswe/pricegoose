418b5f98e1d76be0f711d73e708a816591763da1
Yunxiang Chang <william.chang@uwaterloo.ca>
2020-01-04 21:48:08 -0500

    log in with welcome email

--- site/src/api/login/login.py
+++ site/src/api/login/login.py
@@ -8,14 +8,16 @@ class LoginAPI(APIView):
     def post(self, request):
         try:
             # Log Out current user if anyone is logged in
-            if request.user.is_authenticated:
-                print("already logged in as a user, log out first")
-                logout(request)
-                
+            #if request.user.is_authenticated:
+            #    print("already logged in as a user, log out first")
+            #    logout(request)
+
             # TODO: Currently only handles google login, add more
             google_login(request)
         except KeyError:
             return Response(status=400, data="Error: Login Failed (Code: 10001)")
+        except AttributeError:
+            return Response(status=401, data="Error: Login Failed (Code: 10002)")
         except:
             return Response(status=400, data="Error: Login Failed (Code: 10002)")
             
--- site/src/api/notifications/welcome.py
+++ site/src/api/notifications/welcome.py
@@ -6,18 +6,11 @@ from django.utils.html import strip_tags
 from django.conf import settings
 
 
-class WelcomeEmailAPI(APIView):
-    def post(self, request):
-        # TODO: Add user info to email content
-        welcome_email()
-        return Response(True)
-
-
-def welcome_email():
-    html_message = render_to_string('email_templates/welcome.html', {'context': 'values'})
+def welcome_email(recipient, first_name):
+    html_message = render_to_string('email_templates/welcome.html', {'first_name': first_name})
     plain_message = strip_tags(html_message)
     subject = 'Thank you for registering to our site'
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = [email_from,]
+    recipient_list = [recipient]
     send_mail(subject, plain_message, email_from, recipient_list, html_message=html_message, fail_silently=False)
 
