18f158553d7b90921f6717db83b331891f464253
Yunxiang Chang <william.chang@uwaterloo.ca>
2020-01-04 20:41:13 -0500

    single user google log in done, need to fix switching users

--- site/src/api/login/login.py
+++ site/src/api/login/login.py
@@ -7,11 +7,18 @@ from django.contrib.auth import logout
 class LoginAPI(APIView):
     def post(self, request):
         try:
-            # TODO Currently only handles google login, add more
+            # Log Out current user if anyone is logged in
+            if request.user.is_authenticated:
+                print("already logged in as a user, log out first")
+                logout(request)
+                
+            # TODO: Currently only handles google login, add more
             google_login(request)
         except KeyError:
-            return Response(status=400)
-
+            return Response(status=400, data="Error: Login Failed (Code: 10001)")
+        except:
+            return Response(status=400, data="Error: Login Failed (Code: 10002)")
+            
         return Response(True)
 
 
