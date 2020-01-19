e9997995a3ec4da745ee8cbf5199c603e3184cd0
Yunxiang Chang <william.chang@uwaterloo.ca>
2020-01-18 22:21:31 -0500

    bug fix for login logout

--- site/src/api/login/google_login.py
+++ site/src/api/login/google_login.py
@@ -5,7 +5,8 @@ from src.common.models.user import User
 from src.common.models.third_party_users import GoogleUser
 from src.api.notifications.welcome import welcome_email
 
-DEBUG_PRINT = True
+DEBUG_PRINT = False
+
 
 def google_login(request):
     serializer = TokenSerializer(data=request.data)
@@ -21,11 +22,11 @@ def google_login(request):
     lname = idinfo["family_name"]
     google_id = idinfo["sub"]
     # Other unused info for future
-    #"email_verified": "true"
-    #"name" : "Test User"
-    #"picture": "https://lh4.googleusercontent.com/-kYgzyAWpZzJ/ABCDEFGHI/AAAJKLMNOP/tIXL9Ir44LE/s99-c/photo.jpg"
-    #"locale": "en"
-    
+    # "email_verified": "true"
+    # "name" : "Test User"
+    # "picture": "https://lh4.googleusercontent.com/-kYgzyAWpZzJ/ABCDEFGHI/AAAJKLMNOP/tIXL9Ir44LE/s99-c/photo.jpg"
+    # "locale": "en"
+
     google_user, is_created = GoogleUser.objects.get_or_create(sub=google_id)
     if DEBUG_PRINT:
         print("Google User has been retrieved or created.")
@@ -35,16 +36,16 @@ def google_login(request):
 
         # It's a new user. Create internal User for this user
         user, user_is_created = User.objects.get_or_create(username=email, email=email, first_name=fname, last_name=lname)
-        
+
         if DEBUG_PRINT:
-            print("User has been retrieved or created.")
-        
+            print("User has been created.")
+
         google_user.user = user
         google_user.save()
     else:
         if DEBUG_PRINT:
             print("Existing user, not creating.")
-        
+
         if google_user.user is None:
             raise AttributeError
 
@@ -56,24 +57,23 @@ def google_login(request):
 
         if DEBUG_PRINT:
             print("Existing user info updated.")
-    
 
     if DEBUG_PRINT:
         print("User and google user set up done.")
     # Send welcome email when user first created
-    if is_created == True:
+    if is_created:
         welcome_email(email, fname)
 
-    # TODO fix this since it is not logging in when switching to a new account
     # log in only if user not currently logged in
     if request.user.is_authenticated:
         raise EnvironmentError
 
     if DEBUG_PRINT:
         print("Logging in the user")
+        print(request.user.is_authenticated)
 
-    login(request, user)
+    login(request, google_user.user)
 
     if DEBUG_PRINT:
         print("Google Login done")
-    
+        print(request.user.is_authenticated)
