d197971871ec311107b950ec849a82dfe43f3b16
Yunxiang Chang <william.chang@uwaterloo.ca>
2020-01-18 22:57:22 -0500

    refactor google_login and combine google_auth with google_login

--- site/src/api/login/login.py
+++ site/src/api/login/login.py
@@ -1,4 +1,4 @@
-from .google_login import google_login
+from .google_login import google_auth_login
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import logout
@@ -10,7 +10,7 @@ class LoginAPI(APIView):
     def post(self, request):
         try:
             # TODO: Currently only handles google login, add more
-            google_login(request)
+            google_auth_login(request)
         except KeyError:
             return Response(status=400, data="Error: Login Failed (Code: 10001)")
         except AttributeError:
