c1afd291f48badae9fc051df68141f58c8508e49
Yunxiang Chang <yunxiangchang@gmail.com>
2020-02-16 19:05:16 -0500

    add test to google login test, fix bug in google login

--- site/tests/api/test_google_login.py
+++ site/tests/api/test_google_login.py
@@ -1,4 +1,5 @@
 from django.test import RequestFactory, TestCase
+from django.contrib.auth import logout
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.sessions.middleware import SessionMiddleware
 from src.api.login.google_login import google_login
@@ -11,7 +12,7 @@ class GoogleLoginTestCase(TestCase):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    def test_google_user_created(self):
+    def test_users_created(self):
         # Create an instance of a POST request.
         request = self.factory.post('/api/login', {'token': 'a_fake_token'})
         # Add a session to the request
@@ -22,20 +23,29 @@ class GoogleLoginTestCase(TestCase):
         request.user = AnonymousUser()
 
         # User info
-        email = 'test1@gmail.com'
-        fname = 'FirstName1'
-        lname = 'LastName1'
+        email = 'test@gmail.com'
+        fname = 'FirstName'
+        lname = 'LastName'
         google_id = 'agoogleid1'
 
         google_login(request, email, fname, lname, google_id)
 
-        google_user, is_created = GoogleUser.objects.get_or_create(sub=google_id)
-        self.assertEqual(is_created, False)
+        # Check GoogleUser is created
+        google_user = GoogleUser.objects.get(sub=google_id)
         self.assertEqual(google_user.sub, google_id)
 
-    def test_user_created(self):
+        # Check User is created
+        user = User.objects.get(username=email, email=email, first_name=fname, last_name=lname)
+        self.assertEqual(user.username, email)
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.first_name, fname)
+        self.assertEqual(user.last_name, lname)
+
+    # Checks when user updates their information on google, whether User object
+    #   is updated correctly
+    def test_user_info_updated(self):
         # Create an instance of a POST request.
-        request = self.factory.post('/api/login', {'token': 'another_fake_token'})
+        request = self.factory.post('/api/login', {'token': 'a_fake_token2'})
         # Add a session to the request
         middleware = SessionMiddleware()
         middleware.process_request(request)
@@ -44,15 +54,61 @@ class GoogleLoginTestCase(TestCase):
         request.user = AnonymousUser()
 
         # User info
+        email = 'test@gmail.com'
+        fname = 'FirstName'
+        lname = 'LastName'
+        google_id = 'agoogleid2'
+
+        # Login with the above user info
+        google_login(request, email, fname, lname, google_id)
+        logout(request)
+
+        # Modify User Info
         email = 'test2@gmail.com'
         fname = 'FirstName2'
         lname = 'LastName2'
-        google_id = 'agoogleid2'
 
+        # Login again with new info
         google_login(request, email, fname, lname, google_id)
 
-        user, user_is_created = User.objects.get_or_create(username=email, email=email, first_name=fname, last_name=lname)
-        self.assertEqual(user_is_created, False)
+        # Check User is updated
+        user = User.objects.get(username=email, email=email, first_name=fname, last_name=lname)
+        self.assertEqual(user.username, email)
         self.assertEqual(user.email, email)
         self.assertEqual(user.first_name, fname)
         self.assertEqual(user.last_name, lname)
+
+    # Checks when user attemps login while already logged in, it raises exception
+    def test_double_login_exception(self):
+        # Create an instance of a POST request.
+        request = self.factory.post('/api/login', {'token': 'a_fake_token2'})
+        # Add a session to the request
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+        # Make an anonymous user for the request
+        request.user = AnonymousUser()
+
+        # User info
+        email = 'test@gmail.com'
+        fname = 'FirstName'
+        lname = 'LastName'
+        google_id = 'agoogleid3'
+
+        # Login with the above user info
+        google_login(request, email, fname, lname, google_id)
+
+        # Another User Info
+        email2 = 'test2@gmail.com'
+        fname2 = 'FirstName2'
+        lname2 = 'LastName2'
+        google_id2 = 'agoogleid4'
+
+        try:
+            # Login again
+            google_login(request, email2, fname2, lname2, google_id2)
+
+            # Above login should raise exception EnvironmentError
+            raise Exception
+        except EnvironmentError:
+            pass
