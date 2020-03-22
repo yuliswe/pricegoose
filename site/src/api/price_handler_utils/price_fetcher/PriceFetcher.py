ec9d1b74afb4f32332beb9ad276e23c0b287f316
Jiaming Zhang <jiaming.zhang@shopify.com>
2020-03-22 16:22:21 -0400

    addback price handler (#22)
    * addback price handler

---
+++ site/src/api/price_handler_utils/price_fetcher/PriceFetcher.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class PriceFetcher:
+
+    def __init__(self, url):
+        self.url = url
+        pass
+
+    def parse(self, class_name):
+        req = requests.get(self.url)
+        soup = BeautifulSoup(req.content, "html.parser")
+        soup.find_all(class_=class_name)
---
+++ site/src/api/url_utils/url_parser.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse
+
+from src.api.url_utils.url_info import UrlInfo
+
+# url = 'https://www.canadiantire.ca/en/pdp/dyson-v11-absolute-stick-vacuum-0438226p.html#srp'
+
+
+class UrlParser:
+
+    @staticmethod
+    def parse_url(url):
+        url_parse_ele = urlparse(url)
+        netloc = url_parse_ele[1].split('.')
+        company = netloc[1]
+        nation = netloc[2]
+        url_info = UrlInfo(url, company, nation)
+        return url_info
